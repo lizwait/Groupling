768cbfd7d67cbf8c4b561b4343550fc784f5481e
Liz Wait <elizabeth.m.wait@gmail.com>
2019-03-17 16:31:48 -0500

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import NewEvent from './components/NewEvent';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="d-flex justify-content-center">
-              <h1>Groupling</h1>
-        </div>
-        <div>
-          <form>
-            <div className="form-group">
-              <label htmlFor="pin"></label>
-              <input type="password" className="form-control" id="pin" placeholder="PIN" />
-            </div>
-            <div className="d-flex justify-content-center input-group-append">
-              <button type="button" className="btn btn-light">submit</button>
-            </div>
-            <div className="d-flex justify-content-center" id= "new-event">
-              <button type="button" className="btn btn-dark">new event</button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route exact path='/' component={NewEvent}/>
+          </Switch>
+        </main>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/NewEvent.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class NewEvent extends Component {
+    render() {
+      return (
+        <div>
+          <div className="d-flex justify-content-center">
+                <h1>Groupling</h1>
+          </div>
+          <div>
+            <form>
+              <div className="form-group">
+                <label htmlFor="pin"></label>
+                <input type="password" className="form-control" id="pin" placeholder="PIN" />
+              </div>
+              <div className="d-flex justify-content-center input-group-append">
+                <button type="button" className="btn btn-light">submit</button>
+              </div>
+              <div className="d-flex justify-content-center" id= "new-event">
+                <button type="button" className="btn btn-dark">new event</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      );
+    }
+    pin() {
+      return(
+      100000 + Math.floor(Math.random() * 900000)
+      );
+    }
+  }
+
+  export default NewEvent;
